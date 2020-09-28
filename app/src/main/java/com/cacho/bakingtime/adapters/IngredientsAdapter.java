8d962c15e64504204b2666775f38b7b16b08a930
Cacho <casimiro.chavez@gmail.com>
2020-09-28 15:13:52 +0200

    added ingredients fragment

--- app/src/main/java/com/cacho/bakingtime/adapters/IngredientsAdapter.java
+++ app/src/main/java/com/cacho/bakingtime/adapters/IngredientsAdapter.java
@@ -1,22 +1,23 @@
 package com.cacho.bakingtime.adapters;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.cacho.bakingtime.StepDetailActivity;
+import com.cacho.bakingtime.R;
 import com.cacho.bakingtime.databinding.IngredientRowItemBinding;
 import com.cacho.bakingtime.model.Recipe;
-import com.google.gson.Gson;
 
 import java.util.List;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class IngredientsAdapter extends RecyclerView.Adapter<IngredientsAdapter.IngredientViewHolder> {
    protected List<Recipe.Ingredient> mIngredientsDataset;
+   protected boolean mSelected = false;
 
    public IngredientsAdapter(@NonNull List<Recipe.Ingredient> ingredients){
        mIngredientsDataset = ingredients;
@@ -47,10 +48,16 @@ public class IngredientsAdapter extends RecyclerView.Adapter<IngredientsAdapter.
         public IngredientViewHolder(final IngredientRowItemBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
-            this.binding.getRoot().setOnClickListener(new View.OnClickListener() {
+            this.binding.ingredientCv.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View v) {
-                    //TODO: change colour of the view to indicate you have it now
+                public void onClick(View view) {
+                    if(!mSelected){
+                        view.setBackgroundColor(Color.LTGRAY);
+                        mSelected = true;
+                    } else {
+                        view.setBackgroundColor(Color.WHITE);
+                        mSelected = false;
+                    }
                 }
             });
         }
@@ -60,6 +67,24 @@ public class IngredientsAdapter extends RecyclerView.Adapter<IngredientsAdapter.
          */
         public void bind(Recipe.Ingredient ingredient) {
             binding.setIngredient(ingredient);
+            switch (ingredient.getMeasure()){
+                case "CUP":
+                    binding.iconIv.setBackgroundResource(R.drawable.ic_measuring_cup);
+                    break;
+                case "TBLSP":
+                case "TSP":
+                    binding.iconIv.setBackgroundResource(R.drawable.ic_measuring_spoons);
+                    break;
+                case "K":
+                case "G":
+                case "OZ":
+                    binding.iconIv.setBackgroundResource(R.drawable.ic_scale);
+                    break;
+                case "UNIT":
+                default:
+                    binding.iconIv.setBackgroundResource(R.drawable.ic_ingredients);
+                    break;
+            }
             binding.executePendingBindings();
         }
     }
